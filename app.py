f3dc5a41792ce0239c9479b37c61b2ab8aaab8d3
ByronMoreno <Byron.moreno.dlu@hotmail.com>
2025-11-20 11:44:06 -0500

    Nuevo 1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "¡Hola Mundo desde Flask con Traefik! 🚀"
+    return "Saludos a todos desde Python"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
